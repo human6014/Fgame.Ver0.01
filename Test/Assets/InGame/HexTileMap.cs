0363ad195bf079092402eb84b39f4d252d270505
human6014 <human6014@naver.com>
2021-12-31 06:26:11 +0900

    23
    Create unbreakable tiles

--- Test/Assets/InGame/HexTileMap.cs
+++ Test/Assets/InGame/HexTileMap.cs
@@ -5,56 +5,96 @@ using UnityEngine;
 public class HexTileMap : MonoBehaviour
 {
     public GameObject HexTilePrefab;
-    public PersonTileMap PersonTileMap_script;
     public Transform PersonTileMap_transform;
-
     [SerializeField]
-    int mapWidth,
-        mapHeight;
+    int mapWidth,  //22 
+        mapHeight; //22 (0,0)제외 10타일 길이의 길 생성
 
     private const float tileXOffset = 1.00725f,
                         tileZOffset = 0.87f;
+    string parentTag;
+    int count;
     void Start()
     {
         Debug.Log("HexTileMap Start 시작");
-
+        parentTag = transform.parent.tag;
+        count = 1;
         CreateHexTileMap();
-        PersonTileMap_script.CopyTag();
-
-        Debug.Log("HexTileMap Start 끝");
     }
 
     void CreateHexTileMap()
     {
-        float mapXMin = -mapWidth / 2;
-        float mapXMax = mapWidth / 2;
+        int mapXMin = -mapWidth / 2;
+        int mapXMax = mapWidth / 2;
 
-        float mapZMin = -mapHeight / 2;
-        float mapZMax = mapHeight / 2;
+        int mapZMin = -mapHeight / 2;
+        int mapZMax = mapHeight / 2;
 
-        for (float x = mapXMin; x < mapXMax; x++)
+        Vector3 pos;
+        GameObject TempGo;
+        for (int x = mapXMin; x < mapXMax; x++)
         {
-            for (float z = mapZMin; z < mapZMax; z++)
+            for (int z = mapZMin; z < mapZMax; z++)
             {
-                GameObject TempGo = Instantiate(HexTilePrefab, PersonTileMap_transform);
-                TempGo.tag = "Floor0";
-                Vector3 pos;
+                TempGo = Instantiate(HexTilePrefab, PersonTileMap_transform);
 
-                if (z % 2 == 0) pos = new Vector3(PersonTileMap_transform.position.x + x * tileXOffset, 0, PersonTileMap_transform.position.z + z * tileZOffset);
-                else            pos = new Vector3(PersonTileMap_transform.position.x + x * tileXOffset + tileXOffset / 2, 0, PersonTileMap_transform.position.z + z * tileZOffset);
+                if (z % 2 == 0) pos = new Vector3(PersonTileMap_transform.position.x + x * tileXOffset, 0,
+                                                  PersonTileMap_transform.position.z + z * tileZOffset);
+                else pos =            new Vector3(PersonTileMap_transform.position.x + x * tileXOffset + tileXOffset / 2, 0,
+                                                  PersonTileMap_transform.position.z + z * tileZOffset);
                 StartCoroutine(SetTileInfo(TempGo, x, z, pos));
             }
         }
     }
-    IEnumerator SetTileInfo(GameObject TempGo, float x, float z, Vector3 pos)
+    IEnumerator SetTileInfo(GameObject TempGo, int x, int z, Vector3 pos)
     {
         Debug.Log("HexTileMap SetTileInfo 시작");
 
         TempGo.transform.parent = PersonTileMap_transform;
         TempGo.name = x.ToString() + "," + z.ToString();
+        TagChecking(TempGo, x, z);
+
         yield return new WaitForSeconds(0.000001f);
         TempGo.transform.position = pos;
-
         Debug.Log("HexTileMap SetTileInfo 끝");
+    }
+    void TagChecking(GameObject TempGo, int x, int z)
+    {
+        switch (parentTag)
+        {
+            case "Floor1" when x == z / 2 && z == count:
+                Debug.Log("Floor1");
+                TagChanging(TempGo);
+                break;
+            case "Floor2" when x == (z + 1) / -2 && z == mapHeight / 2 - count + (count % 2 == 0 ? 1 : -1) && z > 0: //입력 값에따라 안될 경우 있음
+                Debug.Log("Floor2");
+                TagChanging(TempGo);
+                break;
+            case "Floor3" when x < 0 && z == 0 && x > mapWidth / -2:
+                Debug.Log("Floor3");
+                TagChanging(TempGo);
+                break;
+            case "Floor4" when x == (z - 1) / 2 && z == mapHeight / -2 + count && x < 0:
+                Debug.Log("Floor4");
+                TagChanging(TempGo);
+                break;
+            case "Floor5" when x == z / -2 && z == -count + (count % 2 == 0 ? 2 : 0) && z >= mapHeight / -2: //더 좋은 식 찾기
+                TagChanging(TempGo);
+                if (z == mapHeight / -2 | z == 0) TempGo.tag = transform.parent.tag;
+                else Debug.Log("Floor5");
+                break;
+            case "Floor6" when x > 0 && z == 0:
+                Debug.Log("Floor6");
+                TagChanging(TempGo);
+                break;
+            default:
+                TempGo.tag = transform.parent.tag;
+                break;
+        }
+    }
+    void TagChanging(GameObject TempGo)
+    {
+        TempGo.tag = "Floor7";
+        count++;
     }
 }
--- Test/Assets/InGame/PersonTileMap.cs
+++ Test/Assets/InGame/PersonTileMap.cs
@@ -7,48 +7,65 @@ public class PersonTileMap : MonoBehaviour
     public GameManager gameManager_script;
     public NetworkManager networkManager_script;
     public SphereCollider sphere;
+
     private MeshCollider meshCollider;
     private new Rigidbody rigidbody;
     private new Renderer renderer;
     float initRadius;
+    bool outPlayer;
     private void Start()
     {
         Debug.Log("PersonTileMap Start시작");
 
         sphere = GetComponent<SphereCollider>();
-        
-        sphere.radius = gameManager_script.myField[1, gameManager_script.playerNum];
+        if (gameObject.CompareTag("Floor7")) sphere.radius = 5;
+        else sphere.radius = gameManager_script.myField[1, gameManager_script.playerNum];
+
         initRadius = sphere.radius;
-        Debug.Log(sphere.radius);
         gameManager_script.playerNum++;
 
         Debug.Log("PersonTileMap Start 끝");
     }
     private void Update()
     {
-        Debug.Log("PersonTileMap FixedUpdate 중");
-        if (sphere.radius >= 0 && networkManager_script.isFull == true) sphere.radius -= Time.deltaTime * Time.time / 1000;
-    }
+        Debug.Log("PersonTileMap Update 중");
+        if (sphere.radius >= 0)
+        {
+            if (networkManager_script.isFull && !gameObject.CompareTag("Floor7")) sphere.radius -= Time.deltaTime * Time.time / 1000;
+        }
+        else
+        {
+            if (outPlayer) return;
+            Transform[] child = GetComponentsInChildren<Transform>();
 
+            foreach (Transform iter in child)
+            {
+                // 부모(this.gameObject)는 삭제 하지 않기 위한 처리
+                if (iter.name != "HexTileMap")
+                {
+                    StartCoroutine(FallWaiting(iter.gameObject));
+                }
+            }
+            outPlayer = true;
+        }
+    }
     private void OnTriggerExit(Collider other)
     {
-        Debug.Log("PersonTileMap OnTriggerExit 시작");
         if (other.CompareTag(tag))
         {
             if (sphere.radius == initRadius) Destroy(other.gameObject);
             else
             {
-                renderer = other.GetComponent<Renderer>();
-                renderer.material.color = new Color(255 / 255f, 25 / 255f, 25 / 255f);
-
-                StartCoroutine(FallWaiting(other));
+                StartCoroutine(FallWaiting(other.gameObject));
             }
         }
-        Debug.Log("PersonTileMap OnTriggerExit 끝");
     }
-    IEnumerator FallWaiting(Collider other)
+    IEnumerator FallWaiting(GameObject other)
     {
+        renderer = other.GetComponent<Renderer>();
+        renderer.material.color = new Color(255 / 255f, 25 / 255f, 25 / 255f);
         yield return new WaitForSeconds(3f); //밑에 순서 중요함
+        if (!other) yield break;
         meshCollider = other.GetComponent<MeshCollider>();
         meshCollider.convex = true;
         meshCollider.isTrigger = true;
@@ -57,15 +74,4 @@ public class PersonTileMap : MonoBehaviour
         rigidbody.isKinematic = false;
         rigidbody.useGravity = true;
     }
-    public void CopyTag()
-    {
-        Debug.Log("PersonTileMap CopyTag 시작");
-
-        Transform[] tran = GetComponentsInChildren<Transform>();
-        foreach (Transform t in tran)
-            t.gameObject.tag = tag;
-
-        Debug.Log("PersonTileMap CopyTag 끝");
-    }
-
 }
